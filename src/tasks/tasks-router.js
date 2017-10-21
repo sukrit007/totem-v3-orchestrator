58a04229728f6c762bbf058e17e9f59feafa9bae
Sukrit Khera <sukrit007@gmail.com>
2017-10-21 14:39:08 -0700

    Add info statement for debuggin step 2 of state machine

--- src/tasks/tasks-router.js
+++ src/tasks/tasks-router.js
@@ -6,12 +6,14 @@
 const
   constants = require('../common/constants'),
   error = require('../services/error'),
+  logger = require('../common/logger'),
   bottle = constants.BOTTLE_CONTAINER;
 
 module.exports.handler = (event, context, callback) => {
 
   let taskEvent = event.event;
 
+  logger.info(`TaskRouter: Handle event(${taskEvent}`);
   let taskHandler = bottle.container[`tasks-event-${taskEvent}`];
   if(!taskHandler) {
     throw new error.EventHandlerNotRegistered();
